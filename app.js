d612da7dfd5b667012e866ee53c37493589bfb83
DipenGediya <4360dipengediya@email.com>
2024-11-29 15:47:21 +0530

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const mongosanitize = require("express-mongo-sanitize");
+const xss = require('xss-clean')
+
 const globalErrorHandler = require("./controllers/error.controller");
 const userRoute = require("./routes/user.route");
 const staticRoute = require("./routes/static.route");
@@ -9,8 +14,26 @@ const bookingRoute = require("./routes/booking.route");
 
 const app = express();
 
+// set security HTTP headers 
+app.use(helmet());
+
+// limit request for same api
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "To many request from this IP, Please try again in a hour!",
+});
+app.use("/api", limiter); 
+
+// Data sanitization against NoSQL query injection
+app.use(mongosanitize())
+
+// Data sanitization against xss(for html code converting string)
+app.use(xss())
+
 //for json body
-app.use(express.json());
+app.use(express.json({limit:'10kb'}));
+
 app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
